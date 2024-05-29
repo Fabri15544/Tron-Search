e51ea940d1af33047cba227c3b2e29d6b3e61f7a
Fabri15544 <51274314+Fabri15544@users.noreply.github.com>
2024-05-29 22:58:34 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
-import pytesseract
-from PIL import Image
 import os
 import json
 import time
 from http.server import SimpleHTTPRequestHandler, HTTPServer
 import threading
+
+from rtspBrute import RtspBrute
+from common import cargar_datos, guardar_datos, eliminar_duplicados, buscar_palabra, actualizar_datos, extraer_texto_desde_imagen
 
 class NoCacheHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
@@ -17,102 +18,9 @@ class NoCacheHandler(SimpleHTTPRequestHandler):
         self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
         super().end_headers()
 
-def cargar_datos():
-    try:
-        with open('datos.json', 'r') as file:
-            return json.load(file)
-    except FileNotFoundError:
-        return []
 
-def guardar_datos(datos):
-    try:
-        with open('datos.json', 'w') as file:
-            json.dump(datos, file, indent=2)
-        print("Datos guardados correctamente.")
-        threading.Timer(10, actualizar_datos).start()
-    except Exception as e:
-        print(f"Error al guardar datos: {e}")
 
-def eliminar_duplicados(datos):
-    datos_filtrados = []
-    diccionario_combinaciones = {}
-
-    for dato in datos:
-        combinacion = (dato["IP"], dato["Puerto"])
-        diccionario_combinaciones[combinacion] = dato
-
-    datos_filtrados = list(diccionario_combinaciones.values())
-
-    return datos_filtrados
-
-def buscar_palabra(banner, servicio):
-    banner_lower = banner.lower()
-    servicios = servicio.lower().split()
-    
-    palabras_no_camara_found = ["Apache2", "apache", "ubuntu", "microsoft-iis", "routeros", "unix"]
-    
-    for palabra in palabras_no_camara_found:
-        if palabra in banner_lower:
-            return "NULL"
-    
-    for palabra in banner_lower.split():
-        if not any(sv in palabra for sv in servicios):
-            if any(keyword in palabra for keyword in ["camera", "model:", "etag:", "webs x-frame-options:", "iemobile", "homepa", "nvr", "hikvision"]):
-                if "www-authenticate: basic realm=\"index.html\"" in banner_lower or "./doc/page/login.asp?_" in banner_lower or  "dnvrs-webs" in banner_lower or "web x-frame-options: sameorigin etag" in banner_lower or "iemobile" in banner_lower or "homepa" in banner_lower or "nvr" in banner_lower:
-                    return "Camara-Hikvision/DVR"
-                elif "ipcam" in banner_lower:
-                    return "Camara-IPCAM"
-                else:
-                    return "Camara-Found"
-            elif 'www-authenticate: basic realm="index.html"' in banner_lower:
-                return "Camara-Authentication-401"
-    
-    return "NULL"
-
-def extraer_texto_desde_imagen(ruta_imagen):
-    try:
-        return pytesseract.image_to_string(Image.open(ruta_imagen))
-    except Exception as e:
-        pass
-
-def actualizar_datos():
-    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-    ruta_capturas = "screenshot/"
-    datos_filtrados = eliminar_duplicados(cargar_datos())
-
-    archivos = os.listdir(ruta_capturas)
-    for archivo in archivos:
-        if archivo.endswith(".png"):
-            ip, puerto = archivo.split('-')
-            puerto = puerto.split('.')[0]  # Eliminar la extensión .png
-
-            # Buscar la entrada correspondiente en los datos filtrados
-            for dato in datos_filtrados:
-                banner = dato["Banner"]
-                servicio = dato["Servicio"]  # Obtener la lista de servicios (de donde sea que la obtengas)
-                camara = buscar_palabra(banner,servicio)  # Llamada a la función buscar_palabra
-                dato["Camara"] = camara  # Asignación del resultado a la clave "Camara"
-                if (dato["IP"] == ip and str(dato["Puerto"]) == puerto) and dato["Banner"] == "\u001b[31munknown\u001b[0m":
-                    ruta_imagen = os.path.join(ruta_capturas, archivo)
-                    if os.path.exists(ruta_imagen):
-                        texto = extraer_texto_desde_imagen(ruta_imagen)
-                        if texto:
-                            print(f"El banner para {dato['IP']}:{dato['Puerto']} fue reemplazado.")
-                            dato["Banner"] = texto
-                        else:
-                            pass
-                    else:
-                        print(f"No se pudo extraer texto de la imagen {ruta_imagen}")
-    try:
-        guardar_datos(datos_filtrados)
-    except Exception as e:
-        print(f"Error al guardar datos: {e}")
-
-if __name__ == '__main__':
-    # Iniciar un hilo para actualizar los datos cada 10 segundos
-    threading.Thread(target=actualizar_datos, daemon=True).start()
-
-    # Iniciar el servidor HTTP
+def start_http_server():
     port = 8080
     server_address = ('', port)
     httpd = HTTPServer(server_address, NoCacheHandler)
@@ -123,3 +31,23 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         print("Servidor detenido.")
         httpd.server_close()
+
+
+if __name__ == '__main__':
+    # Iniciar un hilo para actualizar los datos cada 10 segundos
+    threading.Thread(target=actualizar_datos, daemon=True).start()
+
+    # Iniciar el servidor HTTP en un hilo separado
+    server_thread = threading.Thread(target=start_http_server)
+    server_thread.start()
+
+    # Cargar datos desde el archivo JSON
+    with open('datos.json', 'r') as file:
+        data = json.load(file)
+
+    # Filtrar las entradas para obtener solo las que usan el puerto 554
+    targets = [(entry['IP'], 554) for entry in data if entry['Puerto'] == 554]
+
+    # Iniciar RtspBrute con las direcciones IP y puertos cargados desde el archivo JSON
+    brute = RtspBrute(targets=targets, dictionary_file="diccionario.txt")
+    brute.run()
