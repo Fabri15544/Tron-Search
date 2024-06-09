879a750a63850ebee0f62564fd50c77e5475e9d0
Fabri15544 <51274314+Fabri15544@users.noreply.github.com>
2024-06-10 01:09:04 +0200

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,5 +1,4 @@
 import subprocess
-import threading
 import queue
 
 # Pregunta al usuario si quiere eliminar un número de la última fila o reemplazarla con asteriscos
@@ -45,62 +44,24 @@ for ip in ips:
         ip_parts = ip_parts[:-num_asterisks] + ['*'] * num_asterisks
     modified_ips.append('.'.join(ip_parts))
 
-# Conjunto para almacenar IPs ya procesadas
-processed_ips = set()
-
 # Crear una cola para las IPs
 ip_queue = queue.Queue()
 
 # Llenar la cola con las IPs modificadas
 for ip in modified_ips:
-    if ip not in processed_ips:
-        ip_queue.put(ip)
-        processed_ips.add(ip)  # Agrega la IP al conjunto de IPs procesadas
+    ip_queue.put(ip)
 
-# Semáforo para limitar el número de subprocesos concurrentes
-num_threads = 1  # Número de subprocesos concurrentes
-max_subprocesses = 2  # Número máximo de subprocesos en total
-semaphore = threading.Semaphore(num_threads)
-
-# Contador de subprocesos
-subprocess_count = 0
-
-# Bloqueo para el contador de subprocesos
-counter_lock = threading.Lock()
-
-# Función para procesar las IPs
-def process_ips():
-    global subprocess_count
-    while not ip_queue.empty():
-        ip = ip_queue.get()
-        command = f'python tron.py --search {ip} {extra_command}'
-        with counter_lock:
-            if subprocess_count >= max_subprocesses:
-                ip_queue.task_done()
-                continue
-            subprocess_count += 1
-
-        try:
-            with semaphore:
-                proc = subprocess.Popen(command, shell=True)
-                proc.wait(timeout=2)  # Establece un timeout de 2 segundos
-        except subprocess.TimeoutExpired:
-            proc.kill()
-            print(f"El proceso para la IP {ip} ha sido cerrado automáticamente después de 2 segundos.")
-        finally:
-            with counter_lock:
-                subprocess_count -= 1
-            ip_queue.task_done()
-
-# Crear y lanzar los hilos
-threads = []
-for _ in range(num_threads):
-    thread = threading.Thread(target=process_ips)
-    thread.start()
-    threads.append(thread)
-
-# Esperar a que todos los hilos terminen
-for thread in threads:
-    thread.join()
+# Procesar las IPs
+while not ip_queue.empty():
+    ip = ip_queue.get()
+    command = f'python tron.py --search {ip} {extra_command}'
+    try:
+        proc = subprocess.Popen(command, shell=True)
+        proc.wait(timeout=30)  # Establece un timeout de 2 segundos
+    except subprocess.TimeoutExpired:
+        proc.kill()
+        print(f"El proceso para la IP {ip} ha sido cerrado automáticamente después de 2 segundos.")
+    finally:
+        ip_queue.task_done()
 
 print("Todos los procesos han finalizado.")
