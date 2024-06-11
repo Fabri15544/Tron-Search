36dbacdfac8ca614a05db48a649ec275596fec04
Fabri15544 <51274314+Fabri15544@users.noreply.github.com>
2024-06-11 07:46:33 +0200

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,21 +1,36 @@
 import subprocess
 from multiprocessing import Pool
+import json
 
 def get_user_input():
     while True:
-        choice = input("¿Quieres eliminar un número de la última fila (E) o reemplazarla con asteriscos (A)? (E/A): ").strip().upper()
-        if choice in ['E', 'A']:
+        choice = input("¿Quieres eliminar un número de la última fila (E), reemplazarla con asteriscos (A) o procesar desde JSON (J)? (E/A/J): ").strip().upper()
+        if choice in ['E', 'A', 'J']:
             break
         else:
-            print("Opción no válida. Debe seleccionar 'E' para eliminar un número o 'A' para reemplazar con asteriscos.")
+            print("Opción no válida. Debe seleccionar 'E' para eliminar un número, 'A' para reemplazar con asteriscos o 'J' para procesar desde JSON.")
     
     if choice == 'E':
         num_digits = int(input("¿Cuántos números quieres eliminar de la última fila de la IP? ").strip())
         num_asterisks = 0
-    else:
+    elif choice == 'A':
         num_asterisks = int(input("¿Cuántos asteriscos quieres para los rangos de IP? ").strip())
         num_digits = 0
-    
+    elif choice == 'J':
+        option = input("¿Quieres eliminar un número de la última fila (E) o reemplazarla con asteriscos (A)? (E/A): ").strip().upper()
+        if option == 'E':
+            num_digits = int(input("¿Cuántos números quieres eliminar de la última fila de la IP? ").strip())
+            num_asterisks = 0
+        elif option == 'A':
+            num_asterisks = int(input("¿Cuántos asteriscos quieres para los rangos de IP? ").strip())
+            num_digits = 0
+        else:
+            num_digits = 0
+            num_asterisks = 0
+    else:
+        num_digits = 0
+        num_asterisks = 0
+
     extra_command = input("¿Tienes otro comando para tron.py? (déjalo en blanco si no) ").strip()
     return choice, num_digits, num_asterisks, extra_command
 
@@ -42,12 +57,20 @@ def process_ip(ip, extra_command):
 def main():
     choice, num_digits, num_asterisks, extra_command = get_user_input()
 
-    with open('ips.txt', 'r') as file:
-        ips = file.readlines()
+    if choice == 'J':
+        with open('ips.json', 'r') as file:
+            data = json.load(file)
+            ips = [bucket['key'] for bucket in data['buckets']]
+    else:
+        with open('ips.txt', 'r') as file:
+            ips = file.readlines()
 
-    modified_ips = modify_ips(ips, choice, num_digits, num_asterisks)
+    if choice in ['E', 'A']:
+        modified_ips = modify_ips(ips, choice, num_digits, num_asterisks)
+    else:
+        modified_ips = ips
 
-    if choice == 'A':
+    if choice == 'A' or choice == 'J':
         for ip in modified_ips:
             process_ip(ip, extra_command)
     else:
