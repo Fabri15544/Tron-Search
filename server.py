d9f5fb1e915cd110a8a7a3df99c5681a63c04749
Fabri15544 <51274314+Fabri15544@users.noreply.github.com>
2025-02-15 00:42:33 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import os
+import socket
 import json
 import time
 from http.server import SimpleHTTPRequestHandler, HTTPServer
@@ -19,11 +20,24 @@ class NoCacheHandler(SimpleHTTPRequestHandler):
         self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
         super().end_headers()
 
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        s.connect(('10.254.254.254', 1))
+        ip_address = s.getsockname()[0]
+    except Exception:
+        ip_address = '127.0.0.1'
+    finally:
+        s.close()
+    return ip_address
+
 def start_http_server():
+    ip_address = get_ip_address()  # Obtener la dirección IPv4 local
     port = 8080
-    server_address = ('', port)
+    server_address = (ip_address, port)
     httpd = HTTPServer(server_address, NoCacheHandler)
-    print(f'Servidor iniciado en http://127.0.0.1:{port}')
+    print(f'Servidor iniciado en http://{ip_address}:{port}')
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
